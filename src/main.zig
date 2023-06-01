fc9524712e5de4fef9bbe7fca1bb921fda546df6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-05-31 21:46:54 -0400

    read the header of the header... we're getting there

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,49 @@
 const std = @import("std");
+const valve_types = @import("valve_types.zig");
+
+const DemoReadError = error{
+    TooSmall,
+    BadHeader,
+};
+
+pub fn print_demo(demo: valve_types.DemoHeader) void {
+    std.debug.print();
+    _ = demo;
+}
+
+pub fn assert_header_good(header: valve_types.DemoHeader, allocator: std.mem.Allocator) !void {
+    const hsize = header.header.len;
+    const control_header = try allocator.alloc(u8, hsize);
+    defer allocator.free(control_header);
+
+    @memcpy(control_header, "HL2DEMO0");
+    control_header[hsize - 1] = 0; // add null byte
+
+    std.debug.print("header length: {any}, control_header length: {any}\n", .{ header.header.len, control_header.len });
+    std.debug.print("header.header: {s}\n", .{header.header});
+    for (header.header, control_header) |header_char, control_char| {
+        if (header_char != control_char) {
+            return DemoReadError.BadHeader;
+        }
+    }
+}
+
+pub fn read_dem(relative_path: []const u8, allocator: std.mem.Allocator) !void {
+    const demo_file = try std.fs.cwd().openFile(relative_path, .{});
+    defer demo_file.close();
+
+    const header_size = @sizeOf(valve_types.DemoHeader);
+    var header: [header_size]u8 = undefined;
+    const bytes_read_for_header = try demo_file.read(&header);
+    if (bytes_read_for_header != header_size) {
+        return DemoReadError.TooSmall;
+    }
+
+    const real_header = @bitCast(valve_types.DemoHeader, header);
+    try assert_header_good(real_header, allocator);
+
+    std.debug.print("{any}\n", .{bytes_read_for_header});
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -8,5 +53,7 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    std.debug.print("Arguments: {s}\n", .{@TypeOf(args)});
+    if (args.len > 1) {
+        try read_dem(args[1], allocator);
+    }
 }
--- src/valve_types.zig
+++ src/valve_types.zig
@@ -1,11 +1,11 @@
-pub const DemoHeader = struct {
-    header: [8][]const u8,
+pub const DemoHeader = extern struct {
+    header: [8]u8,
     demo_protocol: i32,
     network_protocol: i32,
-    server_name: [260][]const u8,
-    client_name: [260][]const u8,
-    map_name: [260][]const u8,
-    game_directory: [260][]const u8,
+    server_name: [260]u8,
+    client_name: [260]u8,
+    map_name: [260]u8,
+    game_directory: [260]u8,
     playback_time: f32,
     ticks: i32,
     frames: i32,
@@ -31,7 +31,7 @@ pub const frame_commands_36plus = enum(u8) {
     dem_stringtables = 9,
 };
 
-pub const Packet = struct {
+pub const Packet = extern struct {
     cmd_type: u8,
     unknown: i32,
     tickcount: i32,
@@ -39,7 +39,7 @@ pub const Packet = struct {
     buffer: *[]u8, // where the length should be equal to size_of_packet
 };
 
-pub const Frame = struct {
+pub const Frame = extern struct {
     server_frame: i32,
     client_frame: i32,
     subpacketsize: i32,
