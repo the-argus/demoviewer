b011911628db947ec61c0da0cfe08724d1973990
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-09 09:33:57 -0400

    better settings for CLI arg parsing in both debug and release

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const dem_io = @import("read_dem.zig");
 const demo_debug = @import("demo_debug.zig");
 const builtin = @import("builtin");
 const clap = @import("clap");
+const log = std.log.scoped(.demoviewer);
 
 pub const std_options = struct {
     // log level depends on build mode
@@ -15,21 +16,32 @@ pub const std_options = struct {
 pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help                Display this help and exit.
-        \\-p, --print-header        Print only the header of the specified demo file and exit.
+        \\-p, --printheader         Print only the header of the specified demo file and exit.
         \\<str>...
         \\
     );
-    var diag = clap.Diagnostic{};
-    var parsed_args = clap.parse(clap.Help, &params, clap.parsers.default, .{
-        .diagnostic = &diag,
-    }) catch |err| {
-        // Report useful error and exit
-        diag.report(std.io.getStdErr().writer(), err) catch {};
-        return err;
+
+    var diag: clap.Diagnostic = .{};
+    var parse_options: clap.ParseOptions = .{};
+    if (builtin.mode == .Debug) {
+        diag = clap.Diagnostic{};
+        parse_options = .{ .diagnostic = &diag };
+    }
+
+    var parsed_args = clap.parse(clap.Help, &params, clap.parsers.default, parse_options) catch |err| {
+        return badarg(err, &params, &diag);
     };
     defer parsed_args.deinit();
 
     var program = &read_full_demo;
+
+    if (parsed_args.args.help > 0) {
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    }
+
+    if (parsed_args.args.printheader > 0) {
+        program = read_only_header;
+    }
 
     for (parsed_args.positionals) |positional_arg| {
         return program(positional_arg);
@@ -48,4 +60,16 @@ fn read_only_header(filename: []const u8) !void {
     const demo_file = try dem_io.open_demo(filename);
     const header = try dem_io.read_header(demo_file);
     demo_debug.print_demo_header(header);
+}
+
+/// When building in debug mode, handle arguments that cannot be parsed in this way
+fn badarg(err: anyerror, params: anytype, diag: *clap.Diagnostic) !void {
+    if (builtin.mode == .Debug) {
+        // Report useful error and exit
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    } else {
+        log.err("Unable to parse command line arguments.", .{});
+        return clap.help(std.io.getStdErr().writer(), clap.Help, params, .{});
+    }
 }
