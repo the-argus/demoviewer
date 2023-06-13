8e0201e2059583d64ff217c8e98c37965ee0ec5a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-12 21:31:25 -0400

    cli interface for bsp printing

--- src/bsp/read_bsp.zig
+++ src/bsp/read_bsp.zig
@@ -1,30 +1,7 @@
 const std = @import("std");
-const demo_types = @import("valve_types.zig");
-const Vector = demo_types.Vector;
-
-const HEADER_LUMPS = 64;
-const HEADER_IDENT = (('P' << 24) + ('S' << 16) + ('B' << 8) + 'V');
-const TF2_BSP_VERSION = 20;
-const BSPLump = extern struct {
-    file_offset: i32,
-    len: i32,
-    version: i32,
-    fourCC: [4]u8, // ident code
-};
-const BSPHeader = extern struct {
-    ident: i32 = HEADER_IDENT,
-    version: i32 = TF2_BSP_VERSION,
-    lumps: []BSPLump,
-    mapRevision: i32,
-};
-
-const BSPPlane = extern struct {
-    normal: Vector,
-    dist: f32,
-    type: i32,
-};
+const types = @import("types.zig");
 
 /// a function to read BSPs which currently only prints its findings to log
-pub fn read_bsp(file: std.fs.File) !void {
-    _ = file;
+pub fn read_bsp(relative_path: []const u8) !void {
+    _ = relative_path;
 }
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const read_dem = @import("read_dem.zig").read_dem;
 const dem_io = @import("read_dem.zig");
+const read_bsp = @import("bsp/read_bsp.zig").read_bsp;
 const demo_debug = @import("demo_debug.zig");
 const builtin = @import("builtin");
 const clap = @import("clap");
@@ -13,10 +14,23 @@ pub const std_options = struct {
     pub const logFn = @import("logging.zig").demoviewer_logger;
 };
 
+const Input = struct {
+    print_only_header: bool,
+    print_only_map_info: bool,
+    map_file: ?[]const u8,
+    demo_file: ?[]const u8,
+    tf_path: ?[]const u8,
+};
+
+const default_tf_path = "~/.steam/steam/steamapps/common/Team Fortress 2/tf";
+
 pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help                Display this help and exit.
         \\-p, --printheader         Print only the header of the specified demo file and exit.
+        \\-m, --mapfile <str>       Override the map specified by the demo file.
+        \\-b, --bspinfo             Print the information of the selected map and exit.
+        \\-t, --tfpath <str>        Path to your tf folder.
         \\<str>...
         \\
     );
@@ -33,30 +47,73 @@ pub fn main() !void {
     };
     defer parsed_args.deinit();
 
-    var program = &read_full_demo;
-
+    // print help and exit if asked for
     if (parsed_args.args.help > 0) {
         return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
     }
 
-    if (parsed_args.args.printheader > 0) {
-        program = read_only_header;
-    }
-
-    for (parsed_args.positionals) |positional_arg| {
-        return program(positional_arg);
-    }
-}
-
-fn read_full_demo(filename: []const u8) !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    var input: Input = .{
+        .print_only_header = false,
+        .print_only_map_info = false,
+        .map_file = null,
+        .demo_file = null,
+        .tf_path = default_tf_path,
+    };
+
+    input.print_only_header = parsed_args.args.printheader > 0;
+    input.print_only_map_info = parsed_args.args.bspinfo > 0;
+
+    if (parsed_args.positionals.len > 0) {
+        input.demo_file = parsed_args.positionals[0];
+    } else if (!input.print_only_map_info) {
+        log.err("No positional arguments provided. Need demo file.", .{});
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+    }
+
+    // resolve where the map is
+    if (parsed_args.args.mapfile) |mapfile| {
+        input.map_file = mapfile;
+    } else {
+        if (parsed_args.args.tfpath) |tfpath| {
+            input.tf_path = tfpath;
+        }
+
+        const demo_file = try dem_io.open_demo(input.demo_file.?);
+        const header = try dem_io.read_header(demo_file);
+        const map_name_from_header = header.map_name;
+
+        const map_file_abs_path = try std.fs.path.join(allocator, &[_][]const u8{ input.tf_path.?, &map_name_from_header });
+        const map_file = std.fs.openFileAbsolute(map_file_abs_path, .{ .mode = .read_only }) catch {
+            log.err("Failed to open mapfile {s}", .{map_file_abs_path});
+            log.info("If the attempted path looks wrong, try passing in --tfpath or --mapfile", .{});
+            return;
+        };
+        // don't free the map file abs path so we can pass it around
+        input.map_file = map_file_abs_path;
+        map_file.close();
+    }
+
+    // actual program execution based on input
+    if (input.print_only_map_info) {
+        return read_bsp(input.map_file.?);
+    }
+
+    if (input.print_only_header) {
+        return print_header(input.demo_file.?);
+    }
+
+    return read_full_demo(allocator, input.demo_file.?);
+}
+
+fn read_full_demo(allocator: std.mem.Allocator, filename: []const u8) !void {
     try read_dem(filename, allocator);
 }
 
-fn read_only_header(filename: []const u8) !void {
+fn print_header(filename: []const u8) !void {
     const demo_file = try dem_io.open_demo(filename);
     const header = try dem_io.read_header(demo_file);
     demo_debug.print_demo_header(header);
